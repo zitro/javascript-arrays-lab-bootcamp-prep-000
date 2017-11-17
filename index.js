575c7e3106096b08c27e99084d3ba1d8b37a0109
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-17 17:02:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const app = "I don't do much."
-var kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name){
   kittens.push(name);
   return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
 }
